02f0264044ed5349351c18acaddbfd524bc8860a
atalantadey <atalantadey@2003@gmail.com>
2024-07-17 12:25:21 +0530

    uploading code

---
+++ lib/core/configs/assets/app_images.dart
@@ -0,0 +1,5 @@
+class AppImages {
+  static const String basePath = 'assets/images/';
+  static const String introBG = basePath + 'getStartedBG.png';
+  static const String modeBG = basePath + 'modeBG.png';
+}
