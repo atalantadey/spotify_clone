6de439807b6afeab61167c835f062b3fb4f6236f
atalantadey <atalantadey@2003@gmail.com>
2024-08-03 23:08:36 +0530

    skipandnext

--- lib/common/widgets/fav_btn/fav_btn.dart
+++ lib/common/widgets/fav_btn/fav_btn.dart
@@ -2,19 +2,19 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:spotify/common/bloc/favourite_btn/fav_btn_cubit.dart';
 import 'package:spotify/common/bloc/favourite_btn/fav_btn_state.dart';
-import 'package:spotify/core/configs/theme/app_colors.dart';
+
 import 'package:spotify/domain/entities/song/song.dart';
 
 class FavBtn extends StatelessWidget {
   final SongEntity songEntity;
   final Function? function;
-  const FavBtn({super.key, required this.songEntity,this.function});
+  const FavBtn({super.key, required this.songEntity, this.function});
 
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
       create: (context) => FavBtnCubit(),
-      child: BlocBuilder<FavBtnCubit,FavBtnState>(
+      child: BlocBuilder<FavBtnCubit, FavBtnState>(
         builder: (context, state) {
           if (state is FavBtnInit) {
             print('Fav Btn Init');
@@ -33,14 +33,12 @@ class FavBtn extends StatelessWidget {
           }
           if (state is FavBtnUpdated) {
             print('Fav Btn Updated');
-           return  IconButton(
+            return IconButton(
               onPressed: () {
                 context.read<FavBtnCubit>().favBtnUpdated(songEntity.songId);
               },
               icon: Icon(
-                state.isFav
-                    ? Icons.favorite
-                    : Icons.favorite_outline_outlined,
+                state.isFav ? Icons.favorite : Icons.favorite_outline_outlined,
                 size: 35,
                 color: Colors.red[400],
               ),
